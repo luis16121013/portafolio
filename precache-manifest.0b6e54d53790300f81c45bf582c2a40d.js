0b7aa60bd65243e6716871fcda410366903a440e
LAPC <alguien310599@gmail.com>
2020-09-18 21:42:34 -0500

    Updates

---
+++ precache-manifest.0b6e54d53790300f81c45bf582c2a40d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b01554a079bc6915c393922ca97e4206",
+    "url": "/portafolio/index.html"
+  },
+  {
+    "revision": "d6907660451fbdf74947",
+    "url": "/portafolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30aa2f0ea61258e3c697",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portafolio/static/js/2.f96d775e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6907660451fbdf74947",
+    "url": "/portafolio/static/js/main.919efcaf.chunk.js"
+  },
+  {
+    "revision": "d30650f1ec318e36fc57",
+    "url": "/portafolio/static/js/runtime-main.1d39578f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portafolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
